be9e513af84970578ac2435fe5c88625aa58a15d
Stanislas Nanchen <stan@codesmith.ch>
2022-01-09 07:05:41 +0100

    shell utilities
    Signed-off-by: Stanislas Nanchen <stan@codesmith.ch>

---
+++ src/ch/codesmith/anvil/shell.clj
@@ -0,0 +1,23 @@
+(ns ch.codesmith.anvil.shell
+  (:require [clojure.java.shell :as js])
+  (:import (java.util List)))
+
+
+(defn sh [& args]
+  (let [{:keys [exit out] :as result} (apply js/sh args)]
+    (if (= exit 0)
+      (str/trim out)
+      (throw (ex-info "shell error"
+                      (assoc result
+                        :args args))))))
+
+(defn sh! [& args]
+  "Execute the given shell command and redirect the ouput/error to the standard output error; returns nil."
+  (let [^Process process (.. (ProcessBuilder. ^List args)
+                             (inheritIO)
+                             (start))
+        exit             (.waitFor process)]
+    (when (not= exit 0)
+      (throw (ex-info "shell error"
+                      {:exit 0
+                       :args args})))))
--- test/ch/codesmith/anvil/apps_test.clj
+++ test/ch/codesmith/anvil/apps_test.clj
@@ -1,13 +1,9 @@
 (ns ch.codesmith.anvil.apps-test
   (:require [clojure.test :refer :all]
             [ch.codesmith.anvil.apps :as apps]
+            [ch.codesmith.anvil.shell :as sh]
             [clojure.tools.build.api :as b]
-            [babashka.fs :as fs]
-            [clojure.java.io :as io]
-            [clojure.tools.deps.alpha :as deps]
-            [clojure.string :as str]
-            [clojure.java.shell :as js])
-  (:import (java.util List)))
+            [clojure.string :as str]))
 
 (deftest nondir-full-name-correctness
   (is (= "a" (apps/nondir-full-name "a")))
@@ -33,37 +29,18 @@
 (deftest docker-generator-correctness
   (test-docker-generator false))
 
-(defn sh [& args]
-  (let [{:keys [exit out] :as result} (apply js/sh args)]
-    (if (= exit 0)
-      (str/trim out)
-      (throw (ex-info "shell error"
-                      (assoc result
-                        :args args))))))
-
-(defn sh! [& args]
-  "Execute the given shell command and redirect the ouput/error to the standard output error; returns nil."
-  (let [^Process process (.. (ProcessBuilder. ^List args)
-                             (inheritIO)
-                             (start))
-        exit             (.waitFor process)]
-    (when (not= exit 0)
-      (throw (ex-info "shell error"
-                      {:exit 0
-                       :args args})))))
-
 (defn start-registry! [port]
-  (sh! "docker" "run" "-d" "-p" (str port ":5000") "--name" "registry" "registry:2"))
+  (sh/sh! "docker" "run" "-d" "-p" (str port ":5000") "--name" "registry" "registry:2"))
 
-(defn force-stop-registry! [port]
-  (sh! "docker" "rm" "-f" "registry"))
+(defn force-stop-registry! []
+  (sh/sh! "docker" "rm" "-f" "registry"))
 
 (defn docker-rmi! [tag]
-  (sh! "docker" "rmi" tag))
+  (sh/sh! "docker" "rmi" tag))
 
 (defn registry-images []
   (str/split-lines
-    (sh "docker" "image" "ls" "-q" "localhost:5001/hello")))
+    (sh/sh "docker" "image" "ls" "-q" "localhost:5001/hello")))
 
 (defn rm-registry-images! []
   (doseq [image (registry-images)]
@@ -88,16 +65,16 @@
       ; 2. startup local registry
       (start-registry! port)
       ; 3. build push pull run
-      (sh! "./target/docker-app/docker-build.sh")
-      (sh! "./target/docker-app/docker-push.sh")
+      (sh/sh! "./target/docker-app/docker-build.sh")
+      (sh/sh! "./target/docker-app/docker-push.sh")
       (docker-rmi! app-docker-tag)
-      (sh! "docker" "pull" app-docker-tag)
-      (sh! "docker" "run" "--rm" app-docker-tag)
+      (sh/sh! "docker" "pull" app-docker-tag)
+      (sh/sh! "docker" "run" "--rm" app-docker-tag)
       ; 4. build with lib in registry
       (docker-rmi! app-docker-tag)
       (docker-rmi! lib-docker-tag)
-      (sh! "./target/docker-app/docker-build.sh")
+      (sh/sh! "./target/docker-app/docker-build.sh")
       ; 5. shut down local registry
       (finally
-        (force-stop-registry! port))))
+        (force-stop-registry!))))
   (is true))
