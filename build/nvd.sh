d450e35cac4be842cfb9aa13bac98cfd7ae9d995
Stanislas Nanchen <stan@codesmith.ch>
2022-04-18 12:18:03 +0200

    suppress nvd core.async false positive
    Signed-off-by: Stanislas Nanchen <stan@codesmith.ch>

--- build/nvd.sh
+++ build/nvd.sh
@@ -4,5 +4,5 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 (
     cd "${SCRIPT_DIR}"/../nvd || exit 1
-    clojure -J-Dclojure.main.report=stderr -M -m nvd.task.check "" "$(cd ..; clojure -Spath)"
+    clojure -J-Dclojure.main.report=stderr -M -m nvd.task.check "config.json" "$(cd ..; clojure -Spath)"
 )
